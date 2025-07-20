69c7ba2fa48358045f1f6d28fb10f0e7f5f70d9a
mehedipro3 <mehediratul37@gmail.com>
2025-07-21 01:05:01 +0600

    admin site added

--- index.js
+++ index.js
@@ -66,6 +66,22 @@ async function run() {
       res.send(result);
     })
 
+    app.get(`/users/admin/:email`, verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       //insert email if user doesnt exists : 
