907f8f3a6e92afad6083aa8e180ca49192bd7057
mehedipro3 <mehediratul37@gmail.com>
2025-08-06 01:32:23 +0600

    using aggregate pipeline

--- index.js
+++ index.js
@@ -272,6 +272,45 @@ async function run() {
         orders,
         revenue
       })
+    });
+
+    // using aggregate pipeline 
+    app.get('/order-stats', async (req, res) => {
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind: '$menuItemIds'
+        },
+        {
+          $lookup:
+          {
+            from: 'menu',
+            localField: 'menuItemIds',
+            foreignField: '_id',
+            as: 'menuItems'
+          }
+        },
+        {
+          $unwind: '$menuItems'
+        },
+        {
+          $group: {
+            _id: '$menuItems.category',
+            quantity: { $sum: 1 },
+            revenue: { $sum: '$menuItems.price' }
+          }
+        },
+        {
+          $project:
+          {
+            _id: 0,
+            category: '$_id',
+            quantity: '$quantity',
+            revenue: '$revenue'
+          }
+        }
+      ]).toArray();
+
+      res.send(result);
     })
 
 
